a5967694c5d92a4ff0e519e761a78247a33bf6c6
PachishauD <dzmitrypachishau@outlook.com>
2023-07-19 01:54:29 +0900

    FM-0001/ almost done.

--- test.py
+++ test.py
@@ -1,6 +1,12 @@
 import expressvpn
+import time
+
 from requests import get
+from expressvpn import wrapper
+
 
 for i in range(0, 10):
+    time.sleep(3)
+    wrapper.random_connect(True)
     ip = get("https://api.ipify.org").content.decode("utf-8")
     print(ip)
